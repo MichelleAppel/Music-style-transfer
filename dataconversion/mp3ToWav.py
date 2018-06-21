6dfd8fe7eebd51880be84a83ec2df0bd69064eec
renzovanslooten <renzovanslooten@msn.com>
2018-06-21 11:29:40 +0200

    New mp3 to wav

--- dataconversion/mp3ToWav.py
+++ dataconversion/mp3ToWav.py
@@ -1,29 +1,36 @@
 import os
 import pandas as pd
+import numpy as np
 from pydub import AudioSegment
 
 dataset_storage = "/media/renzo/HDD"
-source = dataset_storage + "/fma_small"
+source = "/media/renzo/Amsterdam on Canvas/fma_large"
 destination = dataset_storage + "/fma_large_wav"
 
-df = pd.read_csv(dataset_storage + "/fma_metadata/tracks.csv", low_memory=False)
+genre_list = ['Electronic', 'Rock', 'Pop', 'Hip-Hop', 'Lo-Fi', 'Jazz', 'Classical']
 counter = 0
 
-for innermap in os.listdir(source):
-    for audio_file in os.listdir(source + "/" + innermap):
-        track_id = str(int(audio_file[:-4]))
-        genre = df.loc[df["Unnamed: 0"] == track_id]["track.7"].item()
-        if innermap == "001" and track_id == "1083":
-            print(genre)
-        if genre in ['Electronic', 'Rock', 'Pop', 'Hip-Hop', 'Lo-Fi', 'Jazz', 'Classical', 'Experimental']:
-            if not os.path.exists(destination + "/" + genre):
-                os.mkdir(destination + "/" + genre)
-            try:
-                sound = AudioSegment.from_mp3(source + "/" + innermap + "/" + audio_file)
-                sound.export(destination + "/" + genre + "/" + genre + "_" + str(counter) + ".wav", format="wav")
-                counter += 1
-            except:
-                print("\nPress Enter to remove: " + innermap + "/" + audio_file)
-                input("Press Ctrl+C to quit!")
-                os.remove(source + "/" + innermap + "/" + audio_file)
-                print("file is removed and the algorithm keeps on running...")
+for genre in genre_list:
+    if not os.path.exists(destination + "/" + genre):
+        os.mkdir(destination + "/" + genre)
+
+df = pd.read_csv(dataset_storage + "/tracks.csv", low_memory=False)
+df = df[pd.notnull(df["track.7"])]
+print(df.loc[df["track.7"] == "Experimental"])
+
+# for innermap in os.listdir(source):
+#     print(innermap)
+#     for audio_file in os.listdir(source + "/" + innermap):
+#         track_id = str(int(audio_file[:-4]))
+#         genre = df.loc[df["Unnamed: 0"] == track_id]["track.7"].item()
+#         if genre in genre_list:
+#             counter += 1
+#             if counter > 31208:
+#                 try:
+#                     sound = AudioSegment.from_mp3(source + "/" + innermap + "/" + audio_file)
+#                     sound.export(destination + "/" + genre + "/" + genre + "_" + str(counter) + ".wav", format="wav")
+#                 except:
+#                     print("\nRemoving: " + innermap + "/" + audio_file)
+#                     os.remove(source + "/" + innermap + "/" + audio_file)
+#                     print("file is removed and the algorithm keeps on running...")
+#                     counter -= 1
