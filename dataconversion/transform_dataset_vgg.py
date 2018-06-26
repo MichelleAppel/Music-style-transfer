27d02b3779213796095cdfc8de075521fdd1aab3
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-06-26 14:15:06 +0200

    vgg dataset

--- dataconversion/transform_dataset_vgg.py
+++ dataconversion/transform_dataset_vgg.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 import scipy.misc
 from tqdm import tqdm
+from random import shuffle
 
 '''
 This method loops through a directory with subdirectories (genres) containing samples in .npy format.
@@ -23,15 +24,21 @@ def transform_dataset(source, destination, color):
 				
 		# retrieve all file names (without hidden files)
 		all_files = [f for f in os.listdir(source+'/'+genre) if not f.startswith('.')]
-
-		for filename in all_files:
+		# shuffle files so have randomness in training-validation-testing data
+		shuffle(all_files)
+		num_files = len(all_files)
+		for file_count, filename in enumerate(all_files):
+								
 			spectrogram = np.load(source+'/'+genre+'/'+filename)
-			new_filename = destination + '/' + str(file_counter).zfill(4) + '_' + genre.lower() + '.jpg'
+			idx = filename.find('_', 0)
+			file_id = filename[idx+1:-9]
+			new_filename = destination+'/'+str(file_counter).zfill(4)+'_'+genre.lower()+'_'+file_id+'.jpg'
 			
 			if color == 'gray':
 				scipy.misc.imsave(new_filename, spectrogram)
 			else:
-				rgb_spectrogram = np.zeros((1024, 256, 3), dtype=np.uint8)
+				h, w = np.shape(spectrogram)
+				rgb_spectrogram = np.zeros((h, w, 3), dtype=np.uint8)
 				rgb_spectrogram[..., 0] = spectrogram * 255 / np.max(spectrogram)
 				rgb_spectrogram[..., 1] = spectrogram * 255 / np.max(spectrogram)
 				rgb_spectrogram[..., 2] = spectrogram * 255 / np.max(spectrogram)
@@ -43,7 +50,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-s', '--source', default=None, required=True)
 	parser.add_argument('-d', '--destination', default=None, required=True)
-	parser.add_argument('-c', '--color', default='gray', required=False)
+	parser.add_argument('-c', '--color', default='rgb', required=False)
 	
 	args = vars(parser.parse_args())
 	transform_dataset(args['source'], args['destination'], args['color'])
