d55057bd4587fd338bdab3df80847dfb40db9172
Michelle Appel <appel.michelle@gmail.com>
2018-06-28 14:22:54 +0200

    generator 4

--- StarGAN/model.py
+++ StarGAN/model.py
@@ -145,6 +145,47 @@ class Generator3(nn.Module):
         x = torch.cat([x, c], dim=1)
         return self.main(x)
 
+class Generator4(nn.Module):
+    """Generator network."""
+
+    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6):
+        super(Generator, self).__init__()
+
+        layers = []
+        layers.append(nn.Conv2d(1 + c_dim, conv_dim, kernel_size=11, stride=1, padding=5, bias=False))
+        layers.append(nn.InstanceNorm2d(conv_dim, affine=True, track_running_stats=True))
+        layers.append(nn.SELU(inplace=True))  # Was ReLU
+
+        # Down-sampling layers.
+        curr_dim = conv_dim
+        for i in range(2):
+            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=6, stride=3, padding=1, bias=False))
+            layers.append(nn.InstanceNorm2d(curr_dim * 2, affine=True, track_running_stats=True))
+            layers.append(nn.SELU(inplace=True))  # Was ReLU
+            curr_dim = curr_dim * 2
+
+        # Bottleneck layers.
+        for i in range(repeat_num):
+            layers.append(ResidualBlock(dim_in=curr_dim, dim_out=curr_dim))
+
+        # Up-sampling layers.
+        for i in range(2):
+            layers.append(nn.ConvTranspose2d(curr_dim, curr_dim // 2, kernel_size=6, stride=3, padding=1, bias=False))
+            layers.append(nn.InstanceNorm2d(curr_dim // 2, affine=True, track_running_stats=True))
+            layers.append(nn.SELU(inplace=True))  # Was ReLU
+            curr_dim = curr_dim // 2
+
+        layers.append(nn.Conv2d(curr_dim, 1, kernel_size=11, stride=1, padding=5, bias=False))
+        layers.append(nn.Tanh())
+        self.main = nn.Sequential(*layers)
+
+    def forward(self, x, c):
+        # Replicate spatially and concatenate domain information.
+        c = c.view(c.size(0), c.size(1), 1, 1)
+        c = c.repeat(1, 1, x.size(2), x.size(3))
+        x = torch.cat([x, c], dim=1)
+        return self.main(x)
+
 class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
 
